fa0652f961845d044d41808a8a4d93143b77ca07
mnlse <nielsenmateusz@gmail.com>
2018-03-08 17:36:12 +0100

    Implemented advertisement create action

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -5,4 +5,20 @@ class AdvertisementsController < ApplicationController
 
     @categories = Category.all.collect { |cat| [cat.name, cat.id] }
   end
+
+  def create
+    @advertisement = Advertisement.new(permitted_params)
+    if @advertisement.save
+      flash[:notice] = "Successfully saved advertisement."
+      redirect_to root_path
+    else
+      flash[:error] = "Failed to save advertisement"
+      render :new
+    end
+  end
+
+  private
+  def permitted_params
+    params.require(:advertisement).permit(:category_id, :title, :description)
+  end
 end

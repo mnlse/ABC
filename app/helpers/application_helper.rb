74691c46db2800464a6d5231b5321a66c287cbd2
mnlse <nielsenmateusz@gmail.com>
2018-03-10 17:57:18 +0100

    Null object pattern for Guest User, Guest user IP localizer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  class GuestUser
+    attr_reader :email, :city_last_known
+    def initialize(ip:, email:)
+      @email = email
+      @city_last_known = ::Geocoder.address(ip)
+    end
+  end
+
+  def current_user
+    super || GuestUser.new(email: "guest@guest.com", ip: request.remote_ip)
+  end
 end

2dbe838d78b2de57a83cfc6926618a5a484930ff
mnlse <nielsenmateusz@gmail.com>
2018-03-11 18:13:21 +0100

    Testing fixed migration on production

--- db/20180307195903_create_advertisements.rb
+++ db/20180307195903_create_advertisements.rb
@@ -2,7 +2,7 @@ class CreateAdvertisements < ActiveRecord::Migration[5.1]
   def change
     create_table :advertisements do |t|
       t.references  :user, foreign_key: true
-      t.references  :category
+      t.references  :category, foreign_key: true
       t.string      :title
       t.text        :description
       t.integer     :price
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180311130147) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "advertisements", force: :cascade do |t|
     t.integer "user_id"
     t.integer "category_id"

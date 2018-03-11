28e36deb86d75dc4d5a9056459e71443b8716f7a
mnlse <nielsenmateusz@gmail.com>
2018-03-11 17:43:51 +0100

    Better seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-Advertisement.create!([
-  {user_id: 13, category_id: 1, title: "test", description: "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest", price: 400, main_image_file_name: "greenland-1703396_1920.jpg", main_image_content_type: "image/jpeg", main_image_file_size: 248276, main_image_updated_at: "2018-03-11 15:22:55", is_promoted: true, is_published: true, city: "Wałbrzych", voivodeship: "Dolnośląskie"}
-])
 Category.create!([
   {name: "Motorization", icon: "la la-automobile"},
   {name: "Real Estate", icon: "la la-home"},
@@ -14,4 +11,7 @@ Category.create!([
   {name: "Music & Education", icon: "la la-music"},
   {name: "Services & Business", icon: "la la-envelope"},
   {name: "Relationships", icon: "la la-venus-mars"}
+])
+Advertisement.create!([
+  {user_id: 13, category_id: 1, title: "test", description: "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest", price: 400, main_image_file_name: "greenland-1703396_1920.jpg", main_image_content_type: "image/jpeg", main_image_file_size: 248276, main_image_updated_at: "2018-03-11 15:22:55", is_promoted: true, is_published: true, city: "Wałbrzych", voivodeship: "Dolnośląskie"}
 ])

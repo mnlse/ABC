20cea54044fb68a8d0fccda86af33ad0532c65fd
mnlse <nielsenmateusz@gmail.com>
2018-03-11 15:54:02 +0100

    Added user profile view page

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,6 +1,8 @@
 class UserProfilesController < ApplicationController
   before_action :set_user_profile, only: [:show, :update]
   def show
+    @user = User.find(params[:id])
+    @user_profile = @user.user_profile
   end
 
   def edit

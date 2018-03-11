fc1de9a51054a2de1be1e880d681ce873e59b9b3
mnlse <nielsenmateusz@gmail.com>
2018-03-11 15:19:35 +0100

    Fixed UserProfile image uploading

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-  has_attached_file :avatar, styles: { original: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
-  validates_attachment :avatar, content_type: { content_type: %w(image/jpeg image/png image/jpg) }
+
+  has_attached_file :avatar_image, styles: { original: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
+  validates_attachment :avatar_image, content_type: { content_type: %w(image/jpeg image/png image/jpg) }
 
   has_many :advertisements, dependent: :destroy
   has_one :user_profile, dependent: :destroy

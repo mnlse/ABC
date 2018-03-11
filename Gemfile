fb97696e1d2bb6b6fbfd38289744703db1d8e9e0
mnlse <nielsenmateusz@gmail.com>
2018-03-11 17:22:10 +0100

    Dumped db to db:seeds

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'seed_dump', '~> 3.2', '>= 3.2.4'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
@@ -61,3 +62,4 @@ gem 'jquery-rails', '~> 4.3'
 gem "paperclip", "~> 5.2.1"
 gem 'cocoon', '~> 1.2', '>= 1.2.11'
 gem 'geocoder', '~> 1.4', '>= 1.4.6'
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Advertisement.create!([
+  {user_id: 13, category_id: 1, title: "test", description: "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest", price: 400, main_image_file_name: "greenland-1703396_1920.jpg", main_image_content_type: "image/jpeg", main_image_file_size: 248276, main_image_updated_at: "2018-03-11 15:22:55", is_promoted: true, is_published: true, city: "Wałbrzych", voivodeship: "Dolnośląskie"}
+])
+Category.create!([
+  {name: "Motorization", icon: "la la-automobile"},
+  {name: "Real Estate", icon: "la la-home"},
+  {name: "Work", icon: "la la-briefcase"},
+  {name: "House & Garden", icon: "la la-wrench"},
+  {name: "Electronics", icon: "la la-cog"},
+  {name: "Fashion", icon: "la la-black-tie"},
+  {name: "Animals", icon: "la la-paw"},
+  {name: "For children", icon: "la la-child"},
+  {name: "Sport & Hobby", icon: "la la-futbol-o"},
+  {name: "Music & Education", icon: "la la-music"},
+  {name: "Services & Business", icon: "la la-envelope"},
+  {name: "Relationships", icon: "la la-venus-mars"}
+])
